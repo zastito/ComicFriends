6cd02317c6d5db39d417bbcad36000b206e711ab
jose <joe.sword93@gmail.com>
2022-09-09 17:50:37 +0200

    Nueva excepción

---
+++ ComicFriends/src/main/java/com/app/ComicFriends/exception/ComicPriceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.app.ComicFriends.exception;
+
+public class ComicPriceNotFoundException extends Exception {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ComicPriceNotFoundException(String msg) {
+        super(msg);
+    }
+}
